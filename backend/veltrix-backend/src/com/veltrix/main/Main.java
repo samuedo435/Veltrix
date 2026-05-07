4d31f2b619e7fcbd715845c5d73c5e241473072c
samuedo435 <samuedo435@gmail.com>
2026-05-07 12:11:40 -0500

    se crea la clase de prueba

---
+++ backend/veltrix-backend/src/com/veltrix/main/Main.java
@@ -0,0 +1,11 @@
+package com.veltrix.main;
+
+import com.veltrix.connection.Conexion;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Conexion.conectar();
+    }
+}
